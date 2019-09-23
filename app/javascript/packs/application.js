cb666932719d7dab1abe8989d2f5373c154f5834
Daniele Deltodesco <deaddread666@gmail.com>
2019-09-23 11:44:58 +0800

    added bootstrap, jquery and stimulus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,18 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import 'stylesheets/application'
+
+import 'bootstrap'
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+// The path you may like to change to under `pack` that path will be `./controllers`
+// but convention will be in `/app/javascript/controllers`
+const context = require.context("controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
 
 require("@rails/ujs").start()
 require("turbolinks").start()
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/example'
   resources :reports
   resources :tronc_records
   resources :employees
