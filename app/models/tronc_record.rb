ed899083b48f96b5b1def4fe3110d05160cff5f8
Daniele Deltodesco <deaddread666@gmail.com>
2019-09-29 14:05:32 +0800

    edited show view for reports

--- app/models/tronc_record.rb
+++ app/models/tronc_record.rb
@@ -64,7 +64,7 @@ class TroncRecord < ApplicationRecord
 
   def self.save_attributes(record)
     if record.user.reports.length.positive?
-      record. week_end = TroncRecord.where(user: record.user).last.week_end + 7
+      record.week_end = TroncRecord.where(user: record.user).last.week_end + 7
     end
     record.tax_due = record.gross_tips / 5
   end
