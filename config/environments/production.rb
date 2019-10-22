aad020bd2ee56fdbb12016fac3074fae49ce7523
Daniele Deltodesco <deaddread666@gmail.com>
2019-10-22 14:13:03 +0700

    changed https to http in config prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "tronc_app_production"
-  config.action_mailer.default_url_options = { host: 'https://troncsystem.com' }
+  config.action_mailer.default_url_options = { host: 'http://troncsystem.com' }
 
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
