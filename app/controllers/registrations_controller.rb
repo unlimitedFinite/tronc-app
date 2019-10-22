74066f923d469771d8d1657c6857953f8802ea5e
Daniele Deltodesco <deaddread666@gmail.com>
2019-10-22 11:22:03 +0700

    prevented error when refreshing sign up page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,8 @@
 class RegistrationsController < Devise::RegistrationsController
-  protected
+  # protected
 
-  def after_inactive_sign_up_path_for(resource)
-    '/users/sign_in' # Or :prefix_to_your_route
-  end
+  # def after_inactive_sign_up_path_for(resource)
+  #   '/users/sign_in' # Or :prefix_to_your_route
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: "registrations" }
+  devise_for :users, :skip => [ :registrations ]
+  devise_scope :user do
+    get 'users/sign_up', to: 'users/registrations#new', as: :new_user_registration
+    post 'users/sign_up', to: 'users/registrations#create', as: :user_registration
+  end
   get 'reports/setup', to: 'reports#setup', as: :setup_path
   get 'reports/print_pdf', to: 'reports#print_pdf'
   get 'tronc_records/employees', to: 'tronc_records#new_employees', as: :new_employees_records
