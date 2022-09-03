7892273bb24702631e8b0b87cf62d49b72462be0
unlimitedFinite <danieledeltodesco@gmail.com>
2022-09-04 00:35:28 +0400

    database loaded

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.default :charset => 'utf-8'
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
