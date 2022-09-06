01c8f2fcd9fc56a8c9736b61db07af126431ecf1
unlimitedFinite <danieledeltodesco@gmail.com>
2022-09-06 18:17:41 +0400

    upgrade to rails 7 complete

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module TroncApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 7.0
     config.autoload_paths += %W(#{config.root}/lib)
 
 
